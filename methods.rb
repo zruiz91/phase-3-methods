6c56a9d46603ba3aa9ef39ec63271031dc430359
Zach Ruiz <powersbooth10@gmail.com>
2022-10-12 12:38:20 -0500

    commit tree 8f0c7e46a85569489fd340d852c260e64ba5b6be

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    return nil unless number.class == Integer
+
+    number / 2
+end
